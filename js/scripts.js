fc4cb3ddd2a1d5368877a56bc4e9586abc0d20c7
JakeAsh22 <jacob.ash1998@gmail.com>
2019-10-25 10:35:28 -0700

    Will add the answers to the bottom of the page

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,28 @@ $(document).ready(function()
   $("form#suggestForm").submit(function(event)
   {
     event.preventDefault();
-    var result = 0;
     var question1 = parseInt($("input:radio[name=choice1]:checked").val());
     var question2 = parseInt($("input:radio[name=choice2]:checked").val());
     var question3 = parseInt($("input:radio[name=choice3]:checked").val());
     var question4 = parseInt($("input:radio[name=choice4]:checked").val());
     var question5 = parseInt($("input:radio[name=choice5]:checked").val());
     var question6 = parseInt($("input:radio[name=choice6]:checked").val());
-    result= calculate(question1,question2,question3,question4,question5,question6);
-    alert("hello" + result);
+    var results= calculate(question1,question2,question3,question4,question5,question6);
+
+    if (results>1)
+    {
+      $("#javaScript").show();
+    }
+    else if (results==0) {
+      $("#cSharp").show();
+    }
+    else if (results<-1)
+    {
+      $("#java").show();
+    }
+    else
+    {
+      $("#error").show();
+    }
   });
 });
