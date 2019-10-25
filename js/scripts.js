76f24cc32cb71964f0cba43a548bfbecd51bb24c
JakeAsh22 <jacob.ash1998@gmail.com>
2019-10-25 10:05:33 -0700

    Will add some javaScript to calculate the correct result

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var calculate = function(choice1,choice2,choice3,choice4,choice5,choice6)
+{
+  return choice1 + choice2 + choice3 + choice4 + choice5 + choice6;
+}
+
+
+$(document).ready(function()
+{
+  ("form#suggestForm").submit(function(event)
+  {
+    var question1 = parseInt($("input:radio[name=choice1]:checked").val());
+    var question2 = parseInt($("input:radio[name=choice2]:checked").val());
+    var question3 = parseInt($("input:radio[name=choice3]:checked").val());
+    var question4 = parseInt($("input:radio[name=choice4]:checked").val());
+    var question5 = parseInt($("input:radio[name=choice5]:checked").val());
+    var question6 = parseInt($("input:radio[name=choice6]:checked").val());
+    var result= calculate(question1,question2,question3,question4,question5,question6);
+    alert("hello" + result);
+  });
+});
